066d1a8a4c1db1c56e45bbeaef624a66c87e1c65
Nura <ainur.saduova10@gmail.com>
2024-05-05 17:09:31 -0400

    change img quantity and initial height for horizontal img

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,30 +2,25 @@ import { toContainElement } from "@testing-library/jest-dom/matchers";
 import React, { useState, useEffect, useRef } from "react";
 import ImageCom from "./Image";
 
-import Image from "./Image";
-
 const imagesList = [
   "freya1",
   "freya2",
   "freya3",
   "freya6",
-  "freya10",
   "freya4",
   "freya5",
-  "freya7",
   "freya8",
   "freya9",
   "freya11",
   "freya12",
   "freya13",
-  "freya14",
   "freya15",
 ];
 
 const Gallery = () => {
   const [rootWidth, setrootWidth] = useState(window.innerWidth);
   const [initialOptions, _] = useState({
-    w: Math.floor(rootWidth / 5),
+    w: rootWidth > 700 ? Math.floor(rootWidth / 4) : rootWidth,
     h: "",
     fit: "contain",
     position: "center",
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
-const ImageCom = ({ image, alt, options }) => {
+const ImageCom = ({ key, image, alt, options }) => {
+  if (key <= 3) options.h = options.w * 0.75;
   const baseUrl = process.env.PUBLIC_URL + ".netlify/images?url=/";
   const imageURL = baseUrl + image + ".jpg&" + new URLSearchParams(options);
 
