fa1bd969f4d8efb343884c7b1d898de6ed5c409b
Nura <ainur.saduova10@gmail.com>
2024-05-06 18:03:35 -0400

    add img view form

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Image.css";
+import WholeImageView from "./WholeImageView";
 
 const ImageCom = ({ idx, image, alt, initOptions }) => {
   const options = { ...initOptions };
@@ -7,10 +8,51 @@ const ImageCom = ({ idx, image, alt, initOptions }) => {
     options.h = Math.floor(options.w * 0.75);
     options.fit = "fill";
   }
+  const [isOpen, setIsOpen] = useState(false);
+  const [scrollH, setScrollH] = useState(0);
   const baseUrl = process.env.PUBLIC_URL + ".netlify/images?url=/";
+  //   change before build
+  const localUrl = "/" + image + ".jpg";
   const imageURL = baseUrl + image + ".jpg&" + new URLSearchParams(options);
 
-  return <img src={imageURL} alt={alt} />;
+  const containerClass = isOpen ? "img-field-fullscreen" : "img-field";
+  const handelImageClick = () => {
+    let body = document.querySelector("body");
+    body.style.overflow = "hidden";
+    const scrollTop =
+      window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : (
+            document.documentElement ||
+            document.body.parentNode ||
+            document.body
+          ).scrollTop;
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    setScrollH(scrollTop);
+    setIsOpen(true);
+  };
+
+  const handleCloseButton = () => {
+    let body = document.querySelector("body");
+    body.style.overflow = "";
+    document.body.scrollTop = document.documentElement.scrollTop = scrollH;
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={containerClass}>
+      {!isOpen ? (
+        <img src={localUrl} alt={alt} onClick={handelImageClick} />
+      ) : (
+        <WholeImageView
+          imgUrl={imageURL}
+          alt={alt}
+          options={options}
+          onClose={handleCloseButton}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ImageCom;
---
+++ src/components/WholeImageView.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./WholeImageView.css";
+
+const WholeImageView = ({ imgUrl, alt, options, onClose }) => {
+  const baseImageURL = imgUrl.split("&")[0];
+  const [params, setParams] = useState({ ...options });
+  const [imageURL, setImageURL] = useState(baseImageURL);
+
+  const handleInputChange = (e) => {
+    setParams({ ...params, [e.target.name]: e.target.value });
+    console.log(params);
+  };
+  const handleButtonClick = () => {
+    setImageURL(baseImageURL + "&" + new URLSearchParams(params));
+    console.log(imageURL);
+  };
+  return (
+    <>
+      <img src={imageURL} alr={alt} />
+      <div className="input-field">
+        <label>
+          Width:
+          <input
+            type="number"
+            value={params.w}
+            name="w"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Height:
+          <input
+            type="number"
+            value={params.h}
+            name="h"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Fit:
+          <select value={params.fit} name="fit" onChange={handleInputChange}>
+            <option value="contain">Contain</option>
+            <option value="cover">Cover</option>
+            <option value="fill">Fill</option>
+          </select>
+        </label>
+        <label>
+          Position:
+          <select
+            value={params.position}
+            name="position"
+            onChange={handleInputChange}
+          >
+            <option value="center">Center</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+            <option value="top">Top</option>
+            <option value="bottom">Bottom</option>
+          </select>
+        </label>
+        <label>
+          Quality (0-100):
+          <input
+            type="number"
+            min="0"
+            max="100"
+            name="quality"
+            value={params.quality ?? 75}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button onClick={handleButtonClick}>Get an Image</button>
+        <button onClick={onClose}>Close X</button>
+      </div>
+    </>
+  );
+};
+
+export default WholeImageView;
